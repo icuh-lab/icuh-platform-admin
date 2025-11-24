1a560d6d2c53fb56a21fc52eec6852a4463e3f56
jeongseok <ljs0429777@gmail.com>
2025-11-24 11:05:30 +0900

    feat: 헬스 컨트롤러 추가

---
+++ src/main/java/re/kr/icuh/icuhplatformadmin/core/api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package re.kr.icuh.icuhplatformadmin.core.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
